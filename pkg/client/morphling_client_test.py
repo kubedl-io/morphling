c9506ffbb6373876c5e6c2d3c9dd7df9dc205e61
lwangbm <lpwangthu@gmail.com>
2021-08-03 16:48:35 +0800

    code refactoring

---
+++ pkg/client/morphling_client_test.py
@@ -0,0 +1,28 @@
+from __future__ import print_function
+
+import api_pb2
+import api_pb2_grpc
+import grpc
+
+
+def main():
+
+    manager_server = "morphling-db-manager:6799"
+    channel_manager = grpc.insecure_channel(manager_server)
+    qps_previous = int(1)
+    print(qps_previous)
+
+    mls = []
+    ml = api_pb2.KeyValue(key='qps', value=str(qps_previous))
+    mls.append(ml)
+
+    stub_ = api_pb2_grpc.DBStub(channel_manager)
+    result = stub_.SaveResult(api_pb2.SaveResultRequest(
+        trial_name="test-trial",
+        namespace="test-namespace",
+        results=mls
+    ), timeout=20)
+
+
+if __name__ == '__main__':
+    main()
---
+++ pkg/storage/backends/interface.go
@@ -0,0 +1,36 @@
+/*
+Copyright 2021 The Alibaba Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package backends
+
+import (
+	api_pb "github.com/alibaba/morphling/api/v1alpha1/grpc_proto/grpc_storage/go"
+)
+
+// ObjectStorageBackend provides a collection of abstract methods to
+// interact with different storage backends, write/read pod and job objects.
+type StorageBackend interface {
+	// Initialize initializes a backend storage service with local or remote database.
+	Initialize() error
+	// Close shutdown backend storage service.
+	Close() error
+	// Name returns backend name.
+	Name() string
+	// SaveTrialResult append or update a pod record to backend.
+	SaveTrialResult(observationLog *api_pb.SaveResultRequest) error
+	// GetTrialResult retrieve a TrialResult from backend.
+	GetTrialResult(request *api_pb.GetResultRequest) (*api_pb.GetResultReply, error)
+}
