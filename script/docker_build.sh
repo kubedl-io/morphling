4d661ec2914c031fc9d8120ab03510e5b16e0cbc
lwangbm <wlphpd0urn>
2021-08-03 14:17:57 +0800

    no message

--- script/docker_build.sh
+++ script/docker_build.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-CONTROLLER_IMG ?= kubedl/morphling-controllers:latest
-DB_MANAGER_IMG ?= kubedl/morphling-database-manager:latest
-UI_IMG ?= kubedl/morphling-ui:latest
-ALGORITHM_IMG ?= kubedl/morphling-algorithm:base
-CLIENT_IMG ?= kubedl/morphling-http-client:demo
+CONTROLLER_IMG=kubedl/morphling-controllers:latest
+DB_MANAGER_IMG=kubedl/morphling-database-manager:latest
+UI_IMG=kubedl/morphling-ui:latest
+ALGORITHM_IMG=kubedl/morphling-algorithm:base
+CLIENT_IMG=kubedl/morphling-http-client:demo
 
 SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/../..
 cd ${SCRIPT_ROOT}
