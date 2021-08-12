da68af5fd4fb2477c670ae1bcf9a351439bbc78d
lwangbm <lpwangthu@gmail.com>
2021-08-12 12:55:02 +0800

    clean python code, modify mnist example
    Signed-off-by: lwangbm lpwangthu@gmail.com
    
    Signed-off-by: lwangbm lpwangthu@gmail.com
    
    Signed-off-by: lwangbm lpwangthu@gmail.com

--- pkg/algorithm/v1alpha1/grid/service.py
+++ pkg/algorithm/v1alpha1/grid/service.py
@@ -2,9 +2,11 @@ import logging
 
 import grpc
 
-from api.v1alpha1.grpc_proto.grpc_algorithm.python3 import api_pb2_grpc, api_pb2
+from api.v1alpha1.grpc_proto.grpc_algorithm.python3 import (api_pb2,
+                                                            api_pb2_grpc)
 from pkg.algorithm.v1alpha1.grid.base_service import BaseSamplingService
 from pkg.algorithm.v1alpha1.internal.base_health_service import HealthServicer
+
 logger = logging.getLogger(__name__)
 
 support_algorithms = ["grid", "random"]
@@ -25,16 +27,28 @@ class BaseService(api_pb2_grpc.SuggestionServicer, HealthServicer):
     def ValidateAlgorithmSettings(self, request, context):
         algorithm_name = request.algorithm_name
         if algorithm_name not in support_algorithms:
-            return _set_validate_context_error(context, "algorithm {} is not supported".format(algorithm_name))
+            return _set_validate_context_error(
+                context, "algorithm {} is not supported".format(algorithm_name)
+            )
         return api_pb2.SamplingValidationResponse()
 
     def GetSuggestions(self, request, context):
 
         if request.algorithm_name in support_algorithms:
             service = BaseSamplingService(request)
-            if request.required_sampling + int(len(request.existing_results)) > min(service.space_size, request.sampling_number_specified):
-                return _set_validate_context_error(context, "space size {} is not enough to provide another {} samplings".format(min(service.space_size, request.sampling_number_specified), request.required_sampling))
+            if request.required_sampling + int(len(request.existing_results)) > min(
+                service.space_size, request.sampling_number_specified
+            ):
+                return _set_validate_context_error(
+                    context,
+                    "space size {} is not enough to provide another {} samplings".format(
+                        min(service.space_size, request.sampling_number_specified),
+                        request.required_sampling,
+                    ),
+                )
             new_assignments = service.get_assignment(request)
             return api_pb2.SamplingResponse(assignments_set=new_assignments)
         else:
-            return _set_validate_context_error(context, "algorithm {} is not supported".format(request.algorithm_name))
+            return _set_validate_context_error(
+                context, "algorithm {} is not supported".format(request.algorithm_name)
+            )
